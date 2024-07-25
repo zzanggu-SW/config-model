99fc9b54b02823a6bed63d221a74e4fe1f4fec94
zzanggu-SW <gyeongmin951@aiofarm.co.kr>
2024-07-25 15:15:18 +0900

    first commit

---
+++ server_config_model/config/__init__.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class PortBaseConfig(BaseModel):
+    port: str = "COM3"
+    baudrate: str = "38400"
+
+
+class InputSerialConfigItem(PortBaseConfig):
+    pass
+
+
+class OutputSerialConfigItem(PortBaseConfig):
+    offset: int = 0
+
+
+class SerialConfig(BaseModel):
+    inputs: List[InputSerialConfigItem] = [InputSerialConfigItem]
+    outputs: List[OutputSerialConfigItem] = [OutputSerialConfigItem]
+
+
+class ArduinoConfig(PortBaseConfig):
+    test_message: str = 'test_message'
+    pass
+
+
+class ProgramConfig(BaseModel):
+    line_count: int = 0
+
+
+class Config(BaseModel):
+    test_status: bool = True
+    program_config: ProgramConfig = ProgramConfig()
+    arduino_config: ArduinoConfig = ArduinoConfig()
+    serial_config: SerialConfig = SerialConfig()
