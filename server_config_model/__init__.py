195fa5bbab8bddd8e44025d5e47daf64ab118994
zzanggu-SW <gyeongmin951@aiofarm.co.kr>
2024-09-02 10:33:06 +0900

    style: isort&black, module import to __init__

--- server_config_model/__init__.py
+++ server_config_model/__init__.py
@@ -1,13 +1,16 @@
-from pydantic import BaseModel, field_validator, ValidationInfo
-from typing import Optional, Union, List
-from enum import Enum
 import datetime
 import json
 import os
 import shutil
+from enum import Enum
+from typing import List, Optional, Union
 
+from pydantic import BaseModel, ValidationInfo, field_validator
+
+from server_config_model.default_models import DefaultResult
 
 __version__ = "v0.1-beta"
+
 
 class EncodingEnum(str, Enum):
     ASCII = "ascii"
@@ -134,27 +137,29 @@ def list_unique_timestamps():
     timestamps = set()
     for filename in os.listdir(backup_dir):
         if filename.startswith("aiofarm_config_"):
-            parts = filename.split('_')
+            parts = filename.split("_")
             if len(parts) > 1:
-                timestamp = parts[-1].split('.')[0]  # Extract the timestamp
+                timestamp = parts[-1].split(".")[0]  # Extract the timestamp
                 timestamps.add(timestamp)
     return sorted(timestamps)
 
 
 def rollback_config(timestamp):
     """Rollback the config files to the state of the given timestamp."""
-    
+
     home_dir = os.path.expanduser("~")
     backup_dir = os.path.join(home_dir, "aiofarm_config_backup")
 
     files_to_rollback = {
-        "aiofarm_config.json": os.path.join(backup_dir, f"aiofarm_config_{timestamp}.json"),
+        "aiofarm_config.json": os.path.join(
+            backup_dir, f"aiofarm_config_{timestamp}.json"
+        ),
         "pyproject.toml": os.path.join(backup_dir, f"pyproject_{timestamp}.toml"),
-        "poetry.lock": os.path.join(backup_dir, f"poetry_{timestamp}.lock")
+        "poetry.lock": os.path.join(backup_dir, f"poetry_{timestamp}.lock"),
     }
-    
+
     home_dir = os.path.expanduser("~")
-    
+
     for original_file, backup_file in files_to_rollback.items():
         target_path = os.path.join(backup_dir, original_file)
         if os.path.exists(backup_file):
@@ -166,14 +171,14 @@ def rollback_config(timestamp):
 
 def backup_config():
     """Backup the aiofarm_config.json, pyproject.toml, and poetry.lock files to the aiofarm_config_backup directory."""
-    
+
     def backup_file(source_file, backup_file):
         if os.path.exists(source_file):
             shutil.copy2(source_file, backup_file)
             print(f"Backup successful: {backup_file}")
         else:
             print(f"Source file does not exist: {source_file}")
-    
+
     home_dir = os.path.expanduser("~")
     backup_dir = os.path.join(home_dir, "aiofarm_config_backup")
 
@@ -183,12 +188,12 @@ def backup_config():
 
     # Generate a unique timestamp
     timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    
+
     # List of files to backup
     files_to_backup = {
         "aiofarm_config.json": os.path.join(home_dir, "aiofarm_config.json"),
         "pyproject.toml": os.path.join(os.getcwd(), "pyproject.toml"),
-        "poetry.lock": os.path.join(os.getcwd(), "poetry.lock")
+        "poetry.lock": os.path.join(os.getcwd(), "poetry.lock"),
     }
 
     # Backup each file
@@ -197,13 +202,13 @@ def backup_config():
         backup_file_path = os.path.join(backup_dir, backup_file_name)
         backup_file(source_file, backup_file_path)
 
+
 def save_config(root_config: RootConfig):
     RootConfig.model_validate(root_config)
     RootConfig(**root_config.model_dump())
-    with open(
-        os.path.expanduser("~/aiofarm_config.json"), "w", encoding="utf-8"
-    ) as f:
+    with open(os.path.expanduser("~/aiofarm_config.json"), "w", encoding="utf-8") as f:
         json.dump(root_config.model_dump(), f, ensure_ascii=False, indent=4)
+
 
 DEFAULT_SERVER_ROOT = RootConfig(
     config_type=ComputerTypeEnum.Server,
@@ -229,7 +234,9 @@ def load_server_root_config() -> RootConfig:
     """Server 설정 프레임과 일치하는 RootConfig를 반환합니다."""
     is_wrong_process = False
     try:
-        with open(os.path.expanduser("~/aiofarm_config.json"), "r", encoding="utf-8") as f:
+        with open(
+            os.path.expanduser("~/aiofarm_config.json"), "r", encoding="utf-8"
+        ) as f:
             data = json.load(f)
         root_config = RootConfig(**data)
         ServerConfig(**root_config.config.model_dump())
@@ -252,7 +259,9 @@ def load_server_root_config() -> RootConfig:
 def load_config() -> RootConfig:
     """Load settings from a JSON file and return a Config object."""
     try:
-        with open(os.path.expanduser("~/aiofarm_config.json"), "r", encoding="utf-8") as f:
+        with open(
+            os.path.expanduser("~/aiofarm_config.json"), "r", encoding="utf-8"
+        ) as f:
             data = json.load(f)
         config = RootConfig.model_validate(data)
     except Exception as e:
